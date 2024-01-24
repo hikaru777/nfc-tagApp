e24682feaa3958d683e2bb9d1eb58e4870e7ba38
HikaruHonda <hikaru.h.developer@gmail.com>
2024-01-24 19:59:40 +0900

    Initial Commit

---
+++ nfc-tagApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  nfc-tagApp
+//
+//  Created by 本田輝 on 2024/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ nfc-tagApp/nfc_tagAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  nfc_tagAppApp.swift
+//  nfc-tagApp
+//
+//  Created by 本田輝 on 2024/01/24.
+//
+
+import SwiftUI
+
+@main
+struct nfc_tagAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
